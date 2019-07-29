02a2ebfa135efb144305a8cd402861865e7ce382
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-29 16:36:11 -0500

    :construction: Bringing in rest_framework and first pass at api

--- build_week/urls.py
+++ build_week/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import routers, serializers, viewsets
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^api-auth/', include('rest_framework.urls')),
+    re_path('api-token-auth/', views.obtain_auth_token)
+
 ]
