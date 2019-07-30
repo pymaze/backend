3ac92c41164d7f3dd29c8cbce44e777b2d8d1ca9
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-30 13:00:53 -0500

    :art: Run migrations, clean sode

--- build_week/rooms/views.py
+++ build_week/rooms/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-class TestView(APIView):
-    def get(self, request):
-        content = {'message': 'This is a test!'}
-        return Response(content)
