7b6b28e651102d39b1506fb266a3dd9ac596684e
ryanboris <ryan.boris@gmail.com>
2019-07-29 22:56:43 -0400

    :card_file_box: Add model for rooms
    Found this model in the LambdaMUD examples and wanted to use it as both just a starting piece as well as a chance to try and connect the dots with the endpoints and models and see how this all works together in order to make better decisions going forward. #5

---
+++ rooms/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
+
+
+class Room(models.Model):
+    title = models.CharField(max_length=50, default="DEFAULT TITLE")
+    description = models.CharField(
+        max_length=500, default="DEFAULT DESCRIPTION")
+    n_to = models.IntegerField(default=0)
+    s_to = models.IntegerField(default=0)
+    e_to = models.IntegerField(default=0)
+    w_to = models.IntegerField(default=0)
+
+    def connectRooms(self, destinationRoom, direction):
+        destinationRoomID = destinationRoom.id
+        try:
+            destinationRoom = Room.objects.get(id=destinationRoomID)
+        except Room.DoesNotExist:
+            print("That room does not exist")
+        else:
+            if direction == "n":
+                self.n_to = destinationRoomID
+            elif direction == "s":
+                self.s_to = destinationRoomID
+            elif direction == "e":
+                self.e_to = destinationRoomID
+            elif direction == "w":
+                self.w_to = destinationRoomID
+            else:
+                print("Invalid direction")
+                return
+            self.save()
