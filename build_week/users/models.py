e08cbcfffe02b56d50eb150884963bb84b8ac4f6
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-30 13:39:41 -0500

    :construction: Create User and UserManager classes

---
+++ build_week/users/models.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+from django.db import models, transaction
+from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager)
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, username, password, **extra_fields):
+        """
+        Creates and saves a User with the given username and password
+        """
+        try:
+            with transaction.atomic():
+                user = self.model(username=username, **extra_fields)
+                user.set_password(password)
+                user.save(using=self.db)
+                return user
+        except:
+            raise
+
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_staff', True)
+        return self._create_user(username, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model
+    with admin-compliant permissions.
+    """
+    objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
