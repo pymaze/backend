3a7c96b04e701d417397cf4a8ab3a7f65cfe8434
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-30 22:46:02 -0500

    :sparkles: LoginSerializer written

--- build_week/users/serializers.py
+++ build_week/users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 
 from .models import User
 
@@ -25,3 +26,39 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # Use the 'create_user' method to create a new user
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=255, read_only=True)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        if username is None:
+            raise serializers.ValidationError(
+                'A username is required to log in.'
+            )
+        if password is None:
+            raise serializers.ValidationError(
+                'A password is required to log in.'
+            )
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'No user with that username and password was found.'
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'This user has been deactivated.'
+            )
+
+        return {
+            'username': user.username,
+            'token': user.token
+        }
