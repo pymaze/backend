468f3b92382f154b180bf2005d5215d83a89580f
ryanboris <ryan.boris@gmail.com>
2019-07-31 22:35:31 -0400

    :bug: Insert missing comma in path list

--- build_week/urls.py
+++ build_week/urls.py
@@ -6,7 +6,7 @@ from rooms import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),
-    path('api/rooms/', views.RoomsView.as_view())
+    path('api/rooms/', views.RoomsView.as_view()),
     path('', views.index)
 
 ]
