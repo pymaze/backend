fd38e691b0b2a750c04ddd31085ecc8d95275a05
Will Schulz <will.schulz29@gmail.com>
2019-08-01 18:59:59 -0400

    squirrel-brained pusher implementation

--- build_week/pusherchat/views.py
+++ build_week/pusherchat/views.py
@@ -5,15 +5,19 @@ from decouple import config
 #decorator to make a function only accessible to registered users
 from django.contrib.auth.decorators import login_required
 
-#import the user library
-from pusher import Pusher
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from pusher import Pusher
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 #instantate the pusher class
 pusher = Pusher(app_id=u'833631', key=u'45a54f303df30a21f792', secret=u'8ddef3ccf6ca852780c9')
-# Create your views here.
+
+
+
 #login required to access this page. will redirect to admin login page.
 
 # @login_required(login_url='/admin/login/')
@@ -22,6 +26,14 @@ pusher = Pusher(app_id=u'833631', key=u'45a54f303df30a21f792', secret=u'8ddef3cc
 
 
 @csrf_exempt
-def broadcast(request):
-    pusher.trigger(u'a_channel', u'an_event', {u'name': request.user.username, u'message': request.POST['message']})
-    return HttpResponse("done");
+class PusherView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def broadcast(request):
+        pusher.trigger(u'a_channel', u'an_event', {u'name': request.user.username, u'message': request.POST['message']})
+        return HttpResponse("done");
+    
+
+
+
+
--- build_week/urls.py
+++ build_week/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from rooms import views
-from django.conf.urls import url
-from django.contrib.auth import views as auth_views
-from pusherchat.views import PuserView
+# from django.conf.urls import url
+# from django.contrib.auth import views as auth_views
+from pusherchat.views import PusherView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),
     path('api/rooms/', views.RoomsView.as_view()),
-    path('', views.RoomsView.index)
+    path('', views.RoomsView.index),
+    path('api/pusher/', PusherView.broadcast)
 
 ]
